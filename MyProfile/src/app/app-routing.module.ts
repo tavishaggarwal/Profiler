e382e0f27bfc72a0ecd18fba895656af258b28dc
Tavish Aggarwal <tavishaggarwal1993@gmail.com>
2017-06-17 20:57:34 +0530

    Updated code to explore Angular

--- MyProfile/src/app/app-routing.module.ts
+++ MyProfile/src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AboutUsComponent} from './about-us/about-us.component';
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
- {
-    path: 'Home',
-    redirectTo: '/',
-    pathMatch: 'full'
-  },
+ {path: '', component: HomeComponent},
   {path: 'About', component: AboutUsComponent}
 ];
 @NgModule({
--- MyProfile/src/app/app.component.ts
+++ MyProfile/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import * as jQuery from 'jquery';
 @Component({
   selector: 'profile-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'profile';
+
+ toggle () {
+    $('#bodyWrapper').toggleClass('showaside backgroundImage');
+    $('#toggle').toggleClass('showaside');
+    $('#menu').toggleClass('hide');
+    $('#header').toggleClass('header hide');
+    $('#bio').toggleClass('hide');
+ }
+
+ adjustWidth () {
+   $('#bodyWrapper').addClass('showaside backgroundImage');
+ }
 }
+
