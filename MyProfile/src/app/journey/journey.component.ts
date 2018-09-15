b315529d9c36b9d32f49b2009e9a20fe3da9535f
Tavish Aggarwal <tavishaggarwal1993@gmail.com>
2018-09-15 13:47:38 +0530

    Updated angular 2 project to angular 6 project

--- MyProfile/src/app/journey/journey.component.ts
+++ MyProfile/src/app/journey/journey.component.ts
@@ -7,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JourneyComponent implements OnInit {
 
+  profesionalAchievements = [
+    {title: 'GGK Tech', description: 'Software Engineer: Aug 2017 - Prsent', position: 'left fadeInLeft',
+    imageSrc: '/assets/ggk.png'},
+
+    {title: 'MAQ Software', description: 'Software Engineer: Jun 2016 – Jul 2017', position: 'right fadeInRight',
+    imageSrc: '/assets/maq.png'},
+
+    {title: 'MAQ Software', description: 'Software Intern: Feb 2016 – May 2016', position: 'right fadeInRight',
+    imageSrc: '/assets/maq.png'},
+
+    {title: 'Tridz', description: 'Internship: Dec 2014 - Jan 2015', position: 'left fadeInLeft',
+    imageSrc: '/assets/tridz.png'},
+  ];
+
+  educationAchievements = [
+    {title: 'SRM University, Chennai', description: 'Bachelor\'s Degree, Software Engineering, 9.5 : 2012 - 2016',
+    position: 'left fadeInLeft', imageSrc: '/assets/srm.png'},
+
+    {title: 'Dav Sr sec School, Chandigarh', description: 'Intermidiate: 2011 – 2012',
+    position: 'right fadeInRight', imageSrc: '/assets/dav.gif'},
+
+    {title: 'KC Public School, Jammu', description: '10th: 2009 - 2010', position: 'left fadeInLeft',
+    imageSrc: '/assets/kc_public.png'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
