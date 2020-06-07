3a4c0daf337d476c32aadf23425cd3be9059a69b
Tavish Aggarwal <tavishaggarwal1993@gmail.com>
2020-06-07 15:12:27 +0530

    Fixed issues in the design

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@
 	    var href = $.attr(this, 'href');
 
 	    $('html, body').animate({
-	        scrollTop: $($.attr(this, 'href')).offset().top - 70
+	        scrollTop: $($.attr(this, 'href')).offset().top
 	    }, 500, function() {
 	    	// window.location.hash = href;
 	    });
